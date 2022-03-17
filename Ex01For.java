d794db4b817f5e555232513c0aa5e469ec3a4a2f
Bibi <ravagnanibeatris@gmail.com>
2022-03-17 18:02:53 -0300

    Exercicio Realizado

---
+++ Ex01For.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ex01For {
+    public static void main(String[] args) {
+        for(int contador = 1; contador> 100;contador ++){
+
+            contador = contador + 1;
+            
+            System.out.printf("%d\n", contador);
+        }
+
+      
+    }
+}
