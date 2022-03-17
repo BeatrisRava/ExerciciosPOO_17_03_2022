f3d469576492b04c9c01257719f938fcf4db18dd
Bibi <ravagnanibeatris@gmail.com>
2022-03-17 17:46:10 -0300

    Exercicio finalizado

---
+++ Ex01While.java
@@ -0,0 +1,9 @@
+public class Ex01While{
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 100){
+            contador = contador + 1;
+            System.out.printf("%d", contador);
+        }
+    }
+}
