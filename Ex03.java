76b92ba1c702403735250cd8d2a1437093e6db8c
Beatris Ravagnani <100382407+BeatrisRava@users.noreply.github.com>
2022-03-24 17:19:07 -0300

    Update Ex03.java

--- Ex03.java
+++ Ex03.java
@@ -5,15 +5,15 @@ public class Ex03 {
       int numero, contador, fatorial;
 
       Scanner leitor = new Scanner (System.in);
-      System.out.printf ("Digite um número para calcular o seu fatorial: ");
+      System.out.printf ("Digite um número: ");
       numero = leitor.nextInt();
       contador = numero;
       fatorial = 1;
-      System.out.printf ("[%d!] ", numero);
+      System.out.printf ("%d", numero);
 
       while (contador > 0){
         System.out.printf (" %d ", contador);
-        System.out.printf (contador > 1 ? " X " : " = ");
+        System.out.printf (contador > 1 ? " x " : " = ");
         fatorial *= contador;
         contador -= 1;
       }
